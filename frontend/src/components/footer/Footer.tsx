ed78075daf7cd6a24758300e06e462c9964e9455
Liya V S <liyavsebastian2003@gmail.com>
2024-07-27 18:49:52 +0530

    main files

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div 
+        style={{
+          width: "100",
+          padding: 20,
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer
